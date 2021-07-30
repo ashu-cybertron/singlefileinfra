8df1afadedc90fa52436d9aa8af7efbe595be004
Ashutosh_K_Sah <64386187+ashu-cybertron@users.noreply.github.com>
2021-07-30 14:08:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-  access_key = "$(var.my-access-key)"
-  secret_key = "$(var.my-secret-key)"
+  shared_credentials_file = "C:\Users\Administrator\.aws\credentials"
+  profile = "default"
 }
 
 #vpc
